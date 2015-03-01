c07344dea2e82938b109082ccbff6298bbf1a819
Ian Czekala <iancze@gmail.com>
2015-02-28 22:55:41 -0500

    Changed from a command line script to hardcoding

--- chmaps.py
+++ chmaps.py
@@ -1,55 +1,157 @@
-import argparse
-parser = argparse.ArgumentParser(description="Plot channel maps.")
-parser.add_argument("file", help="FITS file containing the data to plot.")
-parser.add_argument("nrows", type=int, help="The number of rows to plot.")
-parser.add_argument("ncols", type=int, help="The numeber of columns to plot.")
-parser.add_argument("--arcsec", type=float, default=2.0, help="Half-width of the image.")
-parser.add_argument("--trimchan", type=int, nargs=2, help="Which channels to trim from the ends, if any. Indexed from 0, inclusive. Separated by WHITESPACE.")
-args = parser.parse_args()
-
-nrows = args.nrows
-ncols = args.ncols
-
-# Convert from arcsec to degrees
-radius = args.arcsec/3600.
 
 from astropy.io import fits
 import numpy as np
-import matplotlib.pyplot as plt
-import aplpy
-
 import matplotlib
+import matplotlib.pyplot as plt
+import math
+
 matplotlib.rc("contour", negative_linestyle="dashed")
+matplotlib.rc("axes", linewidth=0.5)
+matplotlib.rc("xtick.major", size=2)
+matplotlib.rc("ytick.major", size=2)
+from matplotlib.ticker import FormatStrFormatter as FSF
+from matplotlib.ticker import MaxNLocator
+from matplotlib.ticker import MultipleLocator
+from matplotlib.patches import Ellipse
 
 # Read the number of channels
-hdu_list = fits.open(args.file)
+hdu_list = fits.open("data.fits")
 hdu = hdu_list[0]
-nchan = hdu.header["NAXIS3"]
+header = hdu.header
+nx = header["NAXIS1"]
+ny = header["NAXIS2"]
+nchan = header["NAXIS3"]
 
-RA = hdu.header["OBSRA"]
-DEC = hdu.header["OBSDEC"]
+assert nx % 2 == 0 and ny % 2 == 0 , "We don't have an even number of pixels, assumptions in the routine are violated."
 
-# Make sure we have the right number of spaces to plot
-if args.trimchan:
-    tlow, thigh = args.trimchan
-    ntrim = tlow + 1 + nchan - thigh
-    assert nrows * ncols >= (nchan - ntrim), "Must specify more axes (nrows * ncols) (={}) than number of channels ({})".format(nrows * ncols, nchan - ntrim)
-    # numpy array is apparently ordered (nstokes, nchan, nx, ny)
-    data = hdu.data[:,tlow:thigh, :, :]
-else:
-    assert nrows * ncols >= nchan, "Must specify more axes (nrows * ncols) (={}) than number of channels ({})".format(nrows * ncols, nchan)
-    data = hdu.data[:,:,:]
+# Midpoints
+mx = nx/2
+my = ny/2
 
-# Determine vmin and vmax from all of the channels, excluding the trimmed ones
-vmin = np.min(data)
-vmax = np.max(data)
+# Phase center in decimal degrees
+RA = header["CRVAL1"]
+DEC = header["CRVAL2"]
+
+# from: http://tdc-www.harvard.edu/wcstools/wcstools.wcs.html
+# CRPIX1 and CRPIX2 are the pixel coordinates of the reference point to which
+# the projection and rotation refer, i.e, integers
+# Indexed from 1, I'm assuming
+# If the image is 256 x 256, CRPIX = 129, then it must be indexed from 1.
+
+# CRVAL1 and CRVAL2 give the center coordinate as right ascension or longitude
+# in decimal degrees.
+
+# CDELT1 and CDELT2 indicate the plate scale in degrees per pixel
+# This means that to get proper RA, DEC coordinates you would need to add a
+# cos(DEC) in there or use some WCS tools. But since we just care about offsets,
+# this should be fine
+
+
+xx = np.arange(header["NAXIS1"])
+yy = np.arange(header["NAXIS2"])
+
+# RA coordinates
+CDELT1 = header["CDELT1"]
+CRPIX1 = header["CRPIX1"] - 1. # Now indexed from 0
+
+# DEC coordinates
+CDELT2 = header["CDELT2"]
+CRPIX2 = header["CRPIX2"] - 1. # Now indexed from 0
+
+# Lay down relative coordinates spanning the image
+# Note that RAs actually goes in decreasing order because of sky convention
+dRAs = (np.arange(nx) - nx/2) * CDELT1
+dDECs = (np.arange(ny) - ny/2) * CDELT2
+
+# Truncation radius
+radius = 2/3600.
+
+# Because DEC increases, this means that we should be using imshow with origin="lower" set.
+
+# Determine the frequencies. Goes in increasing order.
+freq = header["CRVAL3"] + np.arange(header["NAXIS3"]) * header["CDELT3"]
+print(freq)
+
+c_kms = 2.99792458e5 # [km s^-1]
+f0 = 230.538e9 # [Hz]
+# f0 = header["RESTFRQ"] # [Hz]
+
+# Convert all frequencies to velocity [km/s]
+vs = c_kms * (f0 - freq)/f0
+
+# Subtract the systemic velocity (in this case subtract a negative)
+# vs -= -26.09
+
+# Read the beam size and position angle from the header
+BMAJ = 3600. * header["BMAJ"] # [arcsec]
+BMIN = 3600. * header["BMIN"] # [arcsec]
+BPA =  header["BPA"] # degrees East of North
+
+# If the dataset is 82 channels big, trim 11 from either side to get 60 channels
+triml = 11
+trimr = 11
+
+data = hdu.data[0,triml:-trimr]
+model_hdu_list = fits.open("model.fits")
+model = model_hdu_list[0].data[0, triml:-trimr]
+model_hdu_list.close()
+
+resid_hdu_list = fits.open("resid.fits")
+resid = resid_hdu_list[0].data[0, triml:-trimr]
+resid_hdu_list.close()
+
+vs = vs[triml:-trimr]
+
+# Reverse the channels so blueshifted channels appear first
+data = data[::-1]
+model = model[::-1]
+resid = resid[::-1]
+vs = vs[::-1]
+
+
+# Data is now (nchan, ny, nx)
+# Assuming BSCALE=1.0 and BZERO=0.0
+
+# Alternatively
+slice_RA = math.floor(math.fabs(radius/CDELT1))
+slice_DEC = math.floor(math.fabs(radius/CDELT2))
+
+assert slice_RA < mx and slice_DEC < my, "Selected radius must be smaller than half size of image."
+
+# print(np.sum(ind_DEC))
+# print(np.sum(ind_RA))
+# We want to shift to phase center (with an offset) and also truncate the
+# RA and DEC which are outside of our range.
+# For some reason, truncating both dimensions at the same time doesn't work properly.
+# so we have to do them separately
+# data = data[:, ind_DEC, :]
+# data = data[:, :, ind_RA]
+data = data[:, mx-slice_DEC:mx+slice_DEC, mx-slice_RA:mx+slice_RA]
+model = model[:, mx-slice_DEC:mx+slice_DEC, mx-slice_RA:mx+slice_RA]
+resid = resid[:, mx-slice_DEC:mx+slice_DEC, mx-slice_RA:mx+slice_RA]
+
+dRAs = 3600 * dRAs[mx-slice_RA:mx+slice_RA] # [arcsec]
+dDECs = 3600 * dDECs[mx-slice_DEC:mx+slice_DEC] # [arcsec]
+
+ext = (dRAs[0], dRAs[-1], dDECs[0], dDECs[-1]) # [arcsec]
+
+# Determine vmin and vmax from all of the channels, all of the datasets, excluding the trimmed ones
+all_data = np.concatenate((data, model, resid))
+
+vmin = np.min(all_data)
+vmax = np.max(all_data)
+
+vvmax = np.max(np.abs(all_data))
+
+norm = matplotlib.colors.Normalize(-vvmax, vvmax)
 
 # Calculate the list of three-sigma contours to feed to the contour routine
-rms   = 2 * 0.0075
+rms   = 3 * 0.00425
 
 levels = []
 # Add contours from rms to vmin, then reverse
-val = 0
+# We don't want a 0-level contour
+val = -rms
 while val > vmin:
     levels.append(val)
     val -= rms
@@ -61,65 +163,107 @@ while val < vmax:
     levels.append(val)
     val += rms
 
-print(levels)
+print("3 sigma contour levels:", levels)
 
 hdu_list.close()
 
-# Maybe, for now, just specify axes as a multiple of individual plot size.
-ax_size = 1 # in
-margin = 1 # in on all sides
+ax_size = 5.5/12 # in
+margin = 0.5 # in. sides and bottom
+middle_margin = 0.1 # separating panels
+top_margin = 0.1
+nrows = 5
+ncols = 12
 
-fig_width = ax_size * ncols + 2 * margin # in
-fig_height = ax_size * nrows + 2 * margin # in
+panel_width = ax_size * ncols # in
+panel_height = ax_size * nrows # in
+
+fig_width = panel_width + 2 * margin # in
+fig_height = 3 * panel_height + 2 * middle_margin + top_margin + margin# in
 
 dx = ax_size / fig_width
 dy = ax_size / fig_height
 
-# APLPy takes subplot parameters in terms of figure fractions. That means to keep
-# plots square, we will have to adjust the amount by which we shift.
 fig = plt.figure(figsize=(fig_width, fig_height))
 
-chan_offset = 0
+#
+incl = 109.5 # deg
+PA = 141.4 # deg
 
-print("Plotting channel: ")
+major = 2 # [arcsec]
+minor = major * math.cos(incl * np.pi/180.)
+slope = math.tan(PA * np.pi/180.)
 
-# Using DEC, determine the appropriate spacing for 1arcsec in RA
-xspace = 2./np.cos(DEC * np.pi/180.)/3600
+x1s = np.linspace(-minor/2 * math.cos(PA * np.pi/180.), minor/2 * math.cos(PA * np.pi/180.))
+y1s = x1s * slope
 
-for row in range(nrows):
-    for col in range(ncols):
-        chan = row * ncols + col + chan_offset
-        if chan <= tlow or chan >= thigh:
-            chan_offset += 1
-            chan += 1
+x2s = np.linspace(-major/2 * math.sin(PA * np.pi/180.), major/2 * math.sin(PA * np.pi/180.))
+y2s = -x2s / slope
 
-        print(chan)
+def plot(data, panel):
+    '''
+    data being the (nchan, ny, nx) dataset
 
-        xmin = (margin + ax_size * col)/fig_width
-        ymin = (margin + ax_size * row)/fig_height
-        splot = [xmin, ymin, dx, dy]
+    panel being the top, middle, or bottom (0, 1, or 2)
+    '''
+    panel_offset = (middle_margin + panel_height) * panel / fig_height # [figure fraction]
 
-        f = aplpy.FITSFigure(args.file, figure=fig, dimensions=[0,1],
-            slices=[chan, 0], subplot=splot)
-        f.set_auto_refresh(False)
-        f.show_colorscale(vmin=vmin, vmax=vmax)
-        f.show_contour(args.file, dimensions=[0,1], slices=[chan, 0],
-            levels=levels, colors="black", linewidths=0.2)
-        f.recenter(RA, DEC, radius)
+    for row in range(nrows):
+        for col in range(ncols):
+            chan = row * ncols + col
+
+            xmin = (margin + ax_size * col)/fig_width
+            ymin = 1.0 - ((top_margin + ax_size * (row + 1))/fig_height + panel_offset)
+            rect = [xmin, ymin, dx, dy]
+
+            ax = fig.add_axes(rect)
+
+            # Plot image
+            ax.imshow(data[chan], cmap="RdBu", norm=norm, origin="lower", extent=ext)
+
+            # Plot contours
+            ax.contour(data[chan], origin="lower", levels=levels, linewidths=0.2, colors="black", extent=ext)
+
+            # Annotate the velocity
+            ax.annotate("{:.1f}".format(vs[chan]), (0.1, 0.8), xycoords="axes fraction", size=5)
+            if row == 0 and col == 0 and panel == 0:
+                ax.annotate(r"$\textrm{km s}^{-1}$", (0.15, 0.6), xycoords="axes fraction", size=5)
+
+            # Set ticks for every arcsec
+            ax.xaxis.set_major_formatter(FSF("%.0f"))
+            ax.yaxis.set_major_formatter(FSF("%.0f"))
+            ax.xaxis.set_major_locator(MultipleLocator(1.))
+            ax.yaxis.set_major_locator(MultipleLocator(1.))
+
+            # Plot the crosses
+            ax.plot(x1s, y1s, lw=0.1, ls=":", color="0.5")
+            ax.plot(x2s, y2s, lw=0.1, ls=":", color="0.5")
+
+            # Plot the beam
+            if row == (nrows - 1) and col == 0:
+                ax.add_artist(Ellipse(xy=(1, -1), width=BMIN, height=BMAJ, angle=BPA,
+                    facecolor="0.8", linewidth=0.2))
+
+            if row == (nrows - 1) and col == 0 and panel==2:
+                # Actually create axis labels
+                ax.set_xlabel(r"$\Delta \alpha$ ['']")
+                ax.set_ylabel(r"$\Delta \delta$ ['']")
+                ax.tick_params(axis='both', which='major', labelsize=8)
+
+            else:
+                # Hide axis label and tick labels
+                ax.xaxis.set_ticklabels([])
+                ax.yaxis.set_ticklabels([])
 
 
-        f.ticks.set_xspacing(6/3600.) # degrees
-        f.ticks.set_yspacing(2/3600.) # degrees
-        
+plot(data, 0)
+plot(model, 1)
+plot(resid, 2)
 
-        if row == 0 and col == 0:
-            f.tick_labels.set_font(size='xx-small')
-            f.axis_labels.set_font(size='x-small')
-        else:
-            f.hide_xaxis_label()
-            f.hide_yaxis_label()
-            f.hide_xtick_labels()
-            f.hide_ytick_labels()
+left = 0.03
+fig.text(left, 0.85, "data", rotation="vertical")
+fig.text(left, 0.54, "model", rotation="vertical")
+fig.text(left, 0.24, "residual", rotation="vertical")
 
+# Add the side labels
 
 fig.savefig("chmaps.pdf")
